1cc5aa36dc2134041ca2af78f2e49a684a68f362
eTimokhov <47916917+eTimokhov@users.noreply.github.com>
2019-07-14 13:13:09 +0300

    Ch 1.12 SongsManager.deleteAlbumsByArtist()

--- src/dynamusic/SongsManager.java
+++ src/dynamusic/SongsManager.java
@@ -1,15 +1,32 @@
 package dynamusic;
 
 import atg.nucleus.GenericService;
-import atg.repository.Repository;
-import atg.repository.RepositoryException;
+import atg.repository.*;
+import atg.repository.rql.RqlStatement;
 
 public class SongsManager extends GenericService {
 
     private Repository repository;
 
     public void deleteAlbumsByArtist(String artistId) throws RepositoryException {
-
+        if (isLoggingDebug()) {
+            logDebug("deleteAlbumsByArtist called with artistId " + artistId);
+        }
+        MutableRepository repository = (MutableRepository) getRepository();
+        RepositoryView albumView = repository.getView("album");
+        RqlStatement findAlbumsByArtistRql = RqlStatement.parseRqlStatement("artist.id = ?0");
+        Object[] rqlParams = new Object[1];
+        rqlParams[0] = artistId;
+        RepositoryItem[] albums = findAlbumsByArtistRql.executeQuery(albumView, rqlParams);
+        if (albums == null)
+            return;
+        for (RepositoryItem album : albums) {
+            String albumId = album.getRepositoryId();
+            repository.removeItem(albumId, "album");
+            if (isLoggingDebug()) {
+                logDebug("Album with id " + albumId + " deleted");
+            }
+        }
     }
 
     public Repository getRepository() {
